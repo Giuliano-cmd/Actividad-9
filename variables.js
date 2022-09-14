fa721048143ba641b19f65de49b23addd4403b49
Giuliano <giulianogarfagnoli@gmail.com>
2022-09-14 13:27:19 -0300

    Agregado index y variables JS

---
+++ variables.js
@@ -0,0 +1,10 @@
+var pais = "España";
+var continente = "Europa";
+var antigüedad = "2021";
+var pais_y_continente = pais + " " + continente;
+
+pais = "Mexico";
+continente = "Latinoamerica";
+
+console.log(pais, continente, antigüedad);
+alert(pais_y_continente)
